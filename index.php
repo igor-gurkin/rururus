2c299eca9fdd469872257499420efcc204e8a703
Igor Gurkin <gurkin.igor@gmail.com>
2015-09-08 22:25:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- base href=""-->
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="description" content="Сайт о всех русскоязычных сервисах в Будапеште: врачи в Будапеште, представительства, русские вечеринки, русскоязычные няни, парикмахеры, риелторы, адвокатские услуги в Венгрии и многое другое">
+<meta name="keywords" content="русские в Будапеште, русскоязычные в Венгрии, сервис Будапешт, в Будапеште по-русски, риелторы в Будапеште, адвокат Будапешт, няня Будапешт">
+<title>rururus</title>
+<link href="favicon.png" rel="shortcut icon" type="image/vnd.microsoft.icon">
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+<script type="text/javascript" src="script.js"></script>
+<link href="favicon.png" rel="icon" type="image/png">
+<link rel="thumb" href="logo.png">
+<link rel='stylesheet' type='text/css' href='style.css'/>
+<link rel="stylesheet" media="screen and (max-device-width: 800px)" href="styleMobile.css" />
+</head>
+
+<body>
+<?php include_once("analyticstracking.php") ?>
+<div id="menu"><div class="menu-item">Рассказать о своих услугах</div><div class="menu-item">Логин</div><div class="menu-item">Контакты</div></div>
+<div id="container">
+	<div id="logo">
+		<img class="logo" src="img/logo.png">
+	</div>
+	<div id="welcome">Добро пожаловать</div><br>
+	<div style="width: 100%; text-align: center;">
+	<div id="search">
+		<div class="searchText">Я ищу русскоязычного</div>
+			<div class="searchBox">
+				<div class="chooseService-bt">Выберите сервис &#x25BC;</div>
+			</div>
+		<div class="searchText">в Будапеште.</div><br>
+	</div>
+	<div id="services" style="display: none;">
+		<ul class="servicesCol1">
+			<li>адвоката</li>
+			<li>врача</li>
+			<li>гида</li>
+			<li>массажиста</li>
+			<li>механика</li>
+		</ul>
+		<ul class="servicesCol2">
+			<li>няню</li>
+			<li>парикмахера</li>
+			<li>переводчика</li>
+			<li>программиста</li>
+			<li>репетитора</li>
+		</ul>
+		<ul class="servicesCol3">
+			<li>риелтора</li>
+			<li>стоматолога</li>
+			<li>фотографа</li>
+			<li style="background-color: transparent;">&nbsp;</li>
+			<li>другое</li>
+		</ul>
+		<ul class="servicesCol4" style="min-width: 150px;   background-color: rgba(142, 115, 104, 0.59);">
+			<li>Представительства</li>
+			<li>Рестораны</li>
+			<li>Магазины</li>
+			<li>Развлечения</li>
+			<li>Сообщества</li>
+		</ul>
+	</div>
+	</div>
+</div>
+<div id="container2" style="display: none;">
+	<div id="resultsBox">
+		<div class="iimage"><img src="img/sampleface.jpg"></div>
+		<div class="ititle">Название сервиса - Имя Фамилия</div>
+		<div class="idescription"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie elementum nibh, et accumsan magna dictum eu. Nulla volutpat ac diam tincidunt convallis. Quisque ut congue nisl, nec auctor erat. Fusce enim metus, consequat a turpis et, dignissim faucibus augue. Sed nec est nisl. Nunc odio tellus, aliquam nec dignissim vitae, iaculis ut lacus. Nam fringilla a elit in venenatis. Curabitur mollis sem nec elit fringilla, nec tristique massa tristique. Nunc rhoncus neque ut lacus elementum rutrum vel non ipsum. In at nulla in felis tincidunt convallis non id sem. Nulla ac urna eget odio egestas faucibus. Sed vitae pretium lectus, ac suscipit neque.</p>
+<p>Sed at tincidunt neque. Nam rutrum eget metus a consectetur. Quisque porttitor tellus a imperdiet venenatis. Mauris non tortor neque. Phasellus in nisi id elit vestibulum placerat. Vivamus commodo ex accumsan velit tempus, a volutpat nisi tempus. Aenean condimentum augue orci, mattis aliquet est volutpat non. Donec varius ex enim, ac vehicula purus auctor consectetur. Curabitur egestas et massa ut interdum.</p></div>
+		<div class="icontact">Контакты</div>
+	</div>
+</div>
+</body>
+</html>
